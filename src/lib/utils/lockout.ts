4ae1d72d3563f1f6110c8339bbde40d4a6420b28
shiv0403 <shiv0403gupta@gmail.com>
2025-04-28 21:38:25 +0530

    fix: update lockout status

--- src/lib/utils/lockout.ts
+++ src/lib/utils/lockout.ts
@@ -404,6 +404,7 @@ export const evaluateLockoutWinner = async (lockoutId: number) => {
           },
           data: {
             winnerId,
+            status: "completed",
           },
         });
       } else {
