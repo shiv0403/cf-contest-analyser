beb1e10491f8d9631e59ce76e91d7de0ccc4d0ec
shiv0403 <shiv0403gupta@gmail.com>
2025-04-30 17:02:33 +0530

    chore: shared redis

--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -1,5 +1,8 @@
 import { readdirSync } from "fs";
 import { join } from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const jobsPath = join(__dirname, "workers");
 const jobFiles = readdirSync(jobsPath);
