68d5835826ade16161340a5e79492cbde7b0102f
shiv0403 <shiv0403gupta@gmail.com>
2025-04-30 22:53:06 +0530

    fix: redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,15 +1,7 @@
 import IORedis from "ioredis";
 
 const getRedisConnection = () => {
-  if (process.env.NODE_ENV === "production") {
-    // Production (Upstash)
-    return new IORedis(process.env.UPSTASH_REDIS_REST_URL || "", {
-      maxRetriesPerRequest: null,
-    });
-  }
-
-  // Development (Local Redis)
-  return new IORedis(process.env.REDIS_URL || "redis://localhost:6379", {
+  return new IORedis(process.env.REDIS_URL || "", {
     maxRetriesPerRequest: null,
   });
 };
